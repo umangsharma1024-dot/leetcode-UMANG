3f6f999d1a49c535adb52f1e3a852ba5b0ec5205
umangsharma1024-dot <umang.sharma1024@gmail.com>
2026-08-08 15:18:50 +0530

    Time: 0 ms (100%), Space: 22.6 MB (53.72%) - LeetHub

---
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>&arr) {
+        int i=0;
+        for(int j=1;j<arr.size();j++){
+            if(arr[i]!=arr[j]){
+                arr[i+1]=arr[j];
+                i++;
+            }
+        }
+        return i+1;
+    }
+};
